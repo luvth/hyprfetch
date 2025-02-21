ebd3382aefaa4de10c422e37d256c1d5b113664c
luvth <raphftne@gmail.com>
2025-02-21 18:22:22 +0100

    SImplified, removed commentary

--- install.sh
+++ install.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# Check if running as root
-if [ "$EUID" -ne 0 ]; then 
-    echo "Please run as root (with sudo)"
-    exit 1
-fi
+[ "$EUID" -ne 0 ] && echo "Please run as root (with sudo)" && exit 1
 
-# Check for required build tools
-if ! command -v g++ &> /dev/null; then
-    echo "g++ is required but not installed."
-    echo "Please install build-essential or g++"
-    exit 1
-fi
+command -v g++ &> /dev/null || { echo "g++ required but not installed."; exit 1; }
 
-echo "Compiling hyprfetch..."
 g++ -O2 -std=c++17 -Wall -static hyprfetch.cpp -o hyprfetch
 
-if [ $? -ne 0 ]; then
-    echo "Compilation failed!"
-    exit 1
-fi
+[ $? -ne 0 ] && echo "Compilation failed!" && exit 1
 
-echo "Installing to /usr/bin..."
 cp hyprfetch /usr/bin/
 chmod 755 /usr/bin/hyprfetch
-
-echo "Cleaning up..."
 rm hyprfetch
-
+rm -rf ../hyprfetch
 echo "Installation complete! You can now run 'hyprfetch' from anywhere."
